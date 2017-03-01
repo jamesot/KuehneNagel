d80a2d7845cc7b2aac769f703ab59e377b63d2b9
rosteve <rstephenosoro@gmail.com>
2017-03-01 06:34:03 +0300

    Build Up

--- app/src/main/java/com/kuehnenagel/android/Dispatch.java
+++ app/src/main/java/com/kuehnenagel/android/Dispatch.java
@@ -193,11 +193,8 @@ public class Dispatch extends AppCompatActivity implements NavigationView.OnNavi
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
+
             super.onBackPressed();
-        }
+
     }
 }
